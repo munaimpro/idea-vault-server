67baf52b6aeb0be5c93ddf137d863d3d157e614a
Munaim Khan <khanmail2599@gmail.com>
2026-05-18 10:35:15 +0600

    feat: mongodb connect and setup

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config();
+
+const mongodburi = process.env.MONGO_URI;
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(mongodburi, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // Create database and idea collection
+        const db = client.db('ideavault');
+        const ideaCollection = db.collection('ideas');
+
+        // Insert single idea
+        app.post('/idea', async (request, response) => {
+            const ideaData = request.body;
+            const result = await ideaCollection.insertOne(ideaData);
+            response.json(result);
+        })
+
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (request, response) => {
+    response.send('Server is running fine')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+})
